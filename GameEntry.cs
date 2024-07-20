0e3d862b4dc7c93027f9bf4c5ab3e9909e6e5804
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-07-21 02:24:36 +0700

    Add project files.

---
+++ GameEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA {
+    public class GameEntry {
+        
+        public string Name { get; }
+        public int Score { get; }
+
+        
+        public GameEntry(string name, int score) {
+            Name = name;
+            Score = score;
+        }
+
+        
+        public override string ToString() {
+            return $"({Name}, {Score})";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DSA {
+    internal class Program {
+        static void Main(string[] args) {
+            GameEntry e0 = new GameEntry("Joe", 500);
+            GameEntry e1 = new GameEntry("Doe", 400);
+            GameEntry e2 = new GameEntry("Andrew", 300);
+            GameEntry e3 = new GameEntry("Mark", 200);
+
+            Scoreboard board = new Scoreboard(3);
+
+            
+            board.AddEntry(e1);
+            board.AddEntry(e2);
+            board.AddEntry(e3);
+            board.AddEntry(e0);
+
+            board.RemoveEntry(2);
+
+            Console.WriteLine(board.ToString());
+
+        }
+    }
+}
